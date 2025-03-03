92c89a245baa307a86599ab2946e69078b2a19ea
HaniehGharaee <gharaie.hanieh@gmail.com>
2025-03-03 15:16:14 +0330

    setup connect to data base

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGODB_URI),
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
