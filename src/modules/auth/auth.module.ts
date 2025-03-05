de46fc1fc6893d439519d13ba737d4a9d023918c
HaniehGharaee <gharaie.hanieh@gmail.com>
2025-03-05 15:09:21 +0330

    Create module auth

---
+++ src/modules/auth/auth.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+//import { ConfigModule } from '@nestjs/config';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+//import { DatabaseModule } from './database/database.module';
+
+@Module({
+  imports: [
+    //ConfigModule.forRoot(),
+    //DatabaseModule,
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AppModule {}
