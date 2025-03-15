9629dc34ab2744e8ea815c50dd592bba6b735270
HaniehGharaee <gharaie.hanieh@gmail.com>
2025-03-15 22:00:15 +0330

    api create user by admin

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -2,13 +2,15 @@ import {
   Body,
   Res,
   Controller,
+  HttpStatus,
   Post,
   UseGuards,
   UsePipes,
   ValidationPipe,
+  ConflictException,
 } from '@nestjs/common';
 import { UserService } from './user.service';
-import { APP_FILTER } from '@nestjs/core';
+import { Response } from 'express'
 import { ApiTags } from '@nestjs/swagger';
 import { userSwagger } from '../../swagger';
 import { Roles } from './schema/roles.decorator';
@@ -18,13 +20,36 @@ import { createUserDto } from './Dto/create-user.dto';
 @ApiTags('user')
 @UseGuards()
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
   @Post('create')
   @userSwagger('create')
   @Roles('admin')
   @UsePipes(new ValidationPipe({ transform: true }))
   async create(@Body() createUserDto: createUserDto, @Res() res: Response) {
-    const user = await this.userService.createUser(createUserDto)
+    try {
+      const user = await this.userService.createUser(createUserDto)
+      return res.status(HttpStatus.CREATED).json({
+        success: true,
+        message: "",
+        data: user,
+        status: HttpStatus.CREATED,
+      });
+    } catch (error) {
+      if (error instanceof ConflictException) {
+        return res.status(HttpStatus.CONFLICT).json({
+          success: false,
+          message: error.message,
+          status: HttpStatus.CONFLICT,
+        })
+
+      }
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        success: false,
+        message: "",
+        error: error.message,
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+      });
+    }
   }
 }
