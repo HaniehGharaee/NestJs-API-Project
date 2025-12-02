510a50697810483db7a892ea32d8147bff2817c9
HaniehGharaee <gharaie.hanieh@gmail.com>
2025-12-02 15:40:54 +0330

    complete code related to login

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -9,16 +9,24 @@ export class AuthController {
   @Post('Login')
   async login(@Req() req: Request, @Res() res: Response) {
     const { username, password } = req.body;
-    const userAgent = req.headers['user-agent'];
-    const ipAddress = req.ip;
+    const userAgent = req.headers['user-agent']; // read from the header
+    const ipAddress = req.ip; // Read from the network layer,
     const user = await this.authService.validateUser(username, password);
     if (!user)
       return res
         .status(401)
         .json({ success: false, message: 'Invalid credentials' });
-    const tokens = await this.authService.login(user, userAgent as string, ipAddress);
+    const tokens = await this.authService.login(
+      user,
+      userAgent as string,
+      ipAddress,
+    );
     console.log('tokenstokenstokenstokens', tokens);
-    res.cookie('refreshToken', tokens.refreshToken);
+    res.cookie('refreshToken', tokens.refreshToken, {
+      httpOnly: true, //Security: XSS Prevention , Cause: Hacker cannot extract token from JS
+      secure: process.env.NODE_ENV === 'production', //Security: Prevent theft over HTTP, Cause: Send over HTTPS only
+      sameSite: 'lax', //Security: Preventing CSRF, Cause: Balancing Security and Usability
+    });
     return res.json({ success: true, accessToken: tokens.accessToken });
   }
 }
