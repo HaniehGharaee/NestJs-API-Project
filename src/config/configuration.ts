11dff0140282e004c15ac1e0cba26f7c93c04165
HaniehGharaee <gharaie.hanieh@gmail.com>
2026-01-06 16:24:14 +0330

    create category seeder

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,10 +1,16 @@
-
 export default () => ({
   port: parseInt(process.env.PORT!, 10) || 3000,
   nodeEnv: process.env.nodeEnv,
   jwt: {
     secret: process.env.JWT_SECRET || 'default_jwt_secret',
     expiresIn: process.env.JWT_EXPIRES || '20m',
-    refreshExpiresIn: process.env.JWT_REFRESH_EXPIRES || '7d'
+    refreshExpiresIn: process.env.JWT_REFRESH_EXPIRES || '7d',
+  },
+  seed: {
+    categoryDrug: {
+      doc: 'Enable drug category seed',
+      format: Boolean,
+      default: process.env.ENABLE_DRUG_CATEGORY_SEED,
+    },
   },
 });
